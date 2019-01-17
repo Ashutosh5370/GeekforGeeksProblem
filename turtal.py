dfaa59a46b71209c2961d5d75616b472a2bfc7a0
Ashutosh5370 <31342360+Ashutosh5370@users.noreply.github.com>
2019-01-17 11:18:28 +0530

    Add files via upload

---
+++ turtal.py
@@ -0,0 +1,13 @@
+
+#Write a program to take two numbers as input and print their difference if the first number is greater than the second number otherwise print their sum.
+
+
+
+x = int(input())
+
+y = int(input())
+
+if x>y:
+    print(x-y)
+else:
+    print(x+y)
